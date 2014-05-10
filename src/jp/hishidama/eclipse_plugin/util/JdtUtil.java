71d88fdd32829efbad708a34c2c5b5f43b0c0860
hishidama <hishi.dama@asahi.email.ne.jp>
2014-05-10 10:33:43 +0900

    Add getJavaElement()

--- src/jp/hishidama/eclipse_plugin/util/JdtUtil.java
+++ src/jp/hishidama/eclipse_plugin/util/JdtUtil.java
@@ -1,9 +1,15 @@
 package jp.hishidama.eclipse_plugin.util;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.ui.wizards.NewTypeWizardPage;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Composite;
 
 public class JdtUtil {
 
@@ -17,5 +23,62 @@ public class JdtUtil {
 			return null;
 		}
 		return unit;
+	}
+
+	private static class DummyContainerPage extends NewTypeWizardPage {
+
+		public DummyContainerPage() {
+			super(true, "dummy");
+		}
+
+		@Override
+		public void createControl(Composite parent) {
+		}
+
+		@Override
+		public IJavaElement getInitialJavaElement(IStructuredSelection selection) {
+			return super.getInitialJavaElement(selection);
+		}
+
+		public IPackageFragmentRoot getPackageFragmentRoot(IJavaElement element) {
+			super.initContainerPage(element);
+			return super.getPackageFragmentRoot();
+		}
+
+		public String getPackageText(IJavaElement element) {
+			super.initTypePage(element);
+			return super.getPackageText();
+		}
+	}
+
+	public static IJavaElement getJavaElement(IStructuredSelection selection) {
+		return new DummyContainerPage().getInitialJavaElement(selection);
+	}
+
+	public static IPackageFragmentRoot getPackageFragmentRoot(IJavaElement element) {
+		if (element == null) {
+			return null;
+		}
+		return new DummyContainerPage().getPackageFragmentRoot(element);
+	}
+
+	public static String getDirectory(IPackageFragmentRoot root) {
+		if (root == null) {
+			return null;
+		}
+		IProject project = root.getJavaProject().getProject();
+		IPath path = root.getPath().makeRelativeTo(project.getFullPath());
+		return path.toPortableString();
+	}
+
+	public static String getPackage(IJavaElement element) {
+		if (element == null) {
+			return null;
+		}
+		String text = new DummyContainerPage().getPackageText(element);
+		if (text.isEmpty()) {
+			return null;
+		}
+		return text;
 	}
 }
